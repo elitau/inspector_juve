22bbfd406ee9f34cceefc99c4ac9a47e49595e4a
Eduard Litau <eduard.litau@gmail.com>
2013-08-13 00:47:18 +0200

    Do not create fixtures in unit test

--- spec/unit/object_repository_spec.rb
+++ spec/unit/object_repository_spec.rb
@@ -2,14 +2,6 @@ require "spec_helper"
 
 module InspectorJuve
   describe ObjectRepository do
-    before(:each) do
-      run_yardoc_for_fixtures_folder
-    end
-
-    after(:each) do
-      destroy_fixture_yardocs
-    end
-
     it "should return all methods" do
       object_repository = ObjectRepository.new(yardoc_objects_db_path)
       object_repository.all_methods.should_not be_empty
