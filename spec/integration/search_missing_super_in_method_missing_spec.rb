47b2a4b4bad7eefcabf8127cb1759ac1f3a869ec
Eduard Litau <eduard.litau@gmail.com>
2013-08-13 00:21:49 +0200

    spec for missing super in method_missing

---
+++ spec/integration/search_missing_super_in_method_missing_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "Search for missing super in method_missing methods" do
+  include InspectorJuve
+
+  before(:each) do
+    run_yardoc_for_fixtures_folder
+  end
+
+  after(:each) do
+    destroy_fixture_yardocs
+  end
+
+  it "should list access to foreign variables by modules" do
+    localizer = MissingSuperInMethodMissing.new(
+                 object_repository: object_repository_for_fixtures
+               )
+    capture_output_from(localizer) do |output|
+      localizer.search
+      output.should include('Method ClassWithBadMethodMissing#method_missing has no super call')
+    end
+  end
+
+  def object_repository_for_fixtures
+    InspectorJuve::ObjectRepository.new yardoc_objects_db_path
+  end
+end
