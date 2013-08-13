b5a931382e62f4c297d7e11c75820d0edb09efd6
Eduard Litau <eduard.litau@gmail.com>
2013-08-13 14:09:22 +0200

    It's ok to raise NoMethodError by hand

--- lib/inspector_juve/localizer/missing_super_in_meethod_missing_method.rb
+++ lib/inspector_juve/localizer/missing_super_in_meethod_missing_method.rb
@@ -2,7 +2,7 @@ module InspectorJuve
   class MissingSuperInMethodMissing < WeakpointLocalizer
     def search
       all_method_missing_methods.select do |mm_method|
-        unless mm_method.source =~ /super/
+        unless mm_method.source =~ /super|NoMethodError/
           reporter << Weakpoint.new(mm_method, 'has no super call')
         end
       end
